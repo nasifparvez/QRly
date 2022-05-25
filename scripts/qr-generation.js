23b4b0274b857df12639fb890b0fb47ee4ea3bf0
Mohammad Nasif Parvez <parvezmo@sheridancollege.ca>
2022-05-24 20:37:16 -0400

    First Commit

---
+++ scripts/qr-generation.js
@@ -0,0 +1,26 @@
+var qrdata = document.getElementById("input-text");
+const downloadButton = document.querySelector('#download-button');
+
+function generateQR() {
+    if($('#qrcode').is(':empty')){
+        var data = qrdata.value;
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            width: window.innerWidth/6,
+            hieght: window.innerWidth/6}
+        );
+        qrcode.makeCode(data);
+        document.getElementById("qrcode").removeAttribute("hidden");
+        document.getElementById("download-button").removeAttribute("hidden");
+    }else{
+        $("#qrcode").empty();
+        generateQR();
+    }
+    downloadQR();
+}
+
+function downloadQR() {
+    const canvas = qrcode.querySelector('canvas');
+    let canvasUrl = canvas.toDataURL('image/png');
+    $("#download-a").attr("href", canvasUrl)
+    $("#download-a").attr('download', 'qrcode.png');
+}
---
+++ scripts/scan-qr.js
@@ -0,0 +1,76 @@
+const qrInput=document.querySelector('.qr-input');
+const form = qrInput.querySelector('form');
+fileInp = form.querySelector("input"),
+infoText = form.querySelector("p"),
+closeBtn = document.querySelector(".close-button"),
+copyBtn = document.querySelector(".copy-button");
+
+
+  //If user drags file Over QR input area
+  qrInput.addEventListener("dragover", (event)=>{
+    event.preventDefault(); //preventing from default behaviour
+    qrInput.classList.add("active");
+  });
+  //If user leave dragged file from QR input area
+  qrInput.addEventListener("dragleave", ()=>{
+    //qrInput.classList.remove("active");
+  });
+  //If user drop file on QR input area
+  qrInput.addEventListener("drop", (event)=>{
+    event.preventDefault(); 
+    file = event.dataTransfer.files[0];//getting user select file and [0] this means if user select multiple files then we'll select only the first one
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
+});
+
+//Fetch Request to analyze QR code
+function fetchRequest(file,formData){
+    fetch("http://api.qrserver.com/v1/read-qr-code/",{
+        method: 'POST', body: formData
+    }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
+        if(!result) return;
+        document.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
+        qrInput.classList.add("active");
+    }).catch(() => {
+        infoText.innerText = "Couldn't scan QR Code";
+    });
+}
+
+fileInp.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
+});
+
+//Copy to clipboard
+copyBtn.addEventListener("click", () => {
+    let text = document.querySelector("textarea").textContent;
+    navigator.clipboard.writeText(text);
+});
+
+form.addEventListener("click",() => fileInp.click());
+
+//Remove QR input area
+closeBtn.addEventListener("click", () => {
+    qrInput.classList.remove("active")
+    fileInp.value = "";
+    document.querySelector("textarea").innerText = "";
+});
+
+
+//For Camera Scanning
+function onScanSuccess(result) {
+    document.querySelector("textarea").innerText = result;
+}
+function onScanError(errorMessage) {
+  //handle scan error
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanError);
