cb56ed34009d465ebec6bb41d824a947bd021a8f
Nasif Parvez <34200192+nasifparvez@users.noreply.github.com>
2022-06-12 18:21:57 -0400

    Update scan-qr.js

--- scripts/scan-qr.js
+++ scripts/scan-qr.js
@@ -76,4 +76,3 @@ function onScanError(errorMessage) {
 var html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
-
